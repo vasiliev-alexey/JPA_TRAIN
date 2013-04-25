b75a92768ad150b6d40a6f13e83529c3138e444e
vasiliev-alexey <alex_v81@mail.ru>
2013-04-25 16:23:18 +0400

    6

--- JPA_EXAMPLE/src/test/java/com/av/tests/CriteriaSearch.java
+++ JPA_EXAMPLE/src/test/java/com/av/tests/CriteriaSearch.java
@@ -9,11 +9,13 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Root;
-
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import com.av.domain.Department;
 import com.av.domain.Employee;
 
 public class CriteriaSearch {
@@ -26,26 +28,25 @@ public class CriteriaSearch {
 				"/META-INF/spring/app-context.xml");
 		EntityManagerFactory emf = ctx.getBean(EntityManagerFactory.class);
 		EntityManager em = emf.createEntityManager();
-		
-		CriteriaBuilder cb  = em.getCriteriaBuilder();
-		CriteriaQuery <Employee> q = cb.createQuery(Employee.class);
-		Root<Employee> emp = q.from(Employee.class);
-		q.select(emp);
-		q.where(cb.equal(emp.get("lastName"),"VASILIEV"));
-		
-		
-		
-		List <Employee >  list = new ArrayList<Employee>();
-		TypedQuery<Employee>  result = em.createQuery(q);
-		
-		list  = result.getResultList();
-		
-		for (Employee employee : list) {
-			System.out.println(employee);
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Department> q = cb.createQuery(Department.class);
+		Root<Department> dept = q.from(Department.class);
+		Join<Department, Employee> emp = dept.join("emps" , JoinType.INNER);
+
+		q.select(dept).distinct(true);
+		q.where(cb.equal(dept, emp.get("dept"))).where(
+				cb.equal(emp.get("lastName"), "Hartstein"));
+
+		List<Department> list = new ArrayList<Department>();
+		TypedQuery<Department> result = em.createQuery(q);
+
+		list = result.getResultList();
+
+		for (Department department : list) {
+			System.out.println(department);
 		}
-		
-		
-		
+
 	}
 
 }
